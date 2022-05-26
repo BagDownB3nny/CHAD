39c92eca7cb2b39bc42f458d737ed4e3d864ca5c
Darren Sim <e0725166@u.nus.edu>
2022-05-26 23:28:19 +0800

    Add joining of friends servers

---
+++ CHAD/Assets/Scripts/Menu System/JoinServerMenuController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class JoinServerMenuController : MonoBehaviour
+{
+    [Header("Levels to Load")]
+    public string joinRoomScreen;
+    public string backScreen;
+    public TMP_InputField serverIp;
+
+    public void LoadJoinRoom() {
+        SceneManager.LoadScene(joinRoomScreen);
+        NetworkManager.SetGameType(GameType.Client);
+        PlayerClient.instance.SetServerIp(serverIp.text);
+        PlayerClient.instance.ConnectToServer();
+    }
+
+    public void Back() {
+        SceneManager.LoadScene(backScreen);
+    }
+}
