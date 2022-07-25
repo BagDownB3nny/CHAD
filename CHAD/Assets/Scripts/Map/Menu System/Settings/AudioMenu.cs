bc9a42239dd266b86f378d7c6aa3b50f54d53112
Darren Sim <e0725166@u.nus.edu>
2022-07-25 17:21:34 +0800

    Fixed some sound bugs, added audio and soundtrack

--- CHAD/Assets/Scripts/Map/Menu System/Settings/AudioMenu.cs
+++ CHAD/Assets/Scripts/Map/Menu System/Settings/AudioMenu.cs
@@ -12,30 +12,41 @@ public class AudioMenu : MonoBehaviour
     [SerializeField]
     private Slider soundEffectsVolume;
 
+    private bool isAwake = false;
+
     private void Awake()
     {
         masterVolume.value = AudioManager.instance.masterVolume * masterVolume.maxValue;
         musicVolume.value = MusicManager.instance.musicVolume * musicVolume.maxValue;
         soundEffectsVolume.value = SoundManager.instance.soundEffectsVolume * soundEffectsVolume.maxValue;
+        isAwake = true;
     }
 
     public void MasterVolumeSlide()
     {
-        AudioManager.instance.SetVolume(masterVolume.value / masterVolume.maxValue);
-        MusicManager.instance.SetVolume(musicVolume.value / musicVolume.maxValue);
-        SoundManager.instance.SetVolume(soundEffectsVolume.value / soundEffectsVolume.maxValue);
-        SoundManager.instance.PlaySound(Sounds.ButtonPress);
+        if (isAwake) {
+            AudioManager.instance.SetVolume(masterVolume.value / masterVolume.maxValue);
+            MusicManager.instance.SetVolume(musicVolume.value / musicVolume.maxValue);
+            SoundManager.instance.SetVolume(soundEffectsVolume.value / soundEffectsVolume.maxValue);
+            SoundManager.instance.PlaySound(Sounds.ButtonPress);
+        }
     }
 
     public void MusicVolumeSlide()
     {
-        MusicManager.instance.SetVolume(musicVolume.value / musicVolume.maxValue);
-        SoundManager.instance.PlaySound(Sounds.ButtonPress);
+        if (isAwake)
+        {
+            MusicManager.instance.SetVolume(musicVolume.value / musicVolume.maxValue);
+            SoundManager.instance.PlaySound(Sounds.ButtonPress);
+        }
     }
 
     public void SoundEffectsVolumeSlide()
     {
-        SoundManager.instance.SetVolume(soundEffectsVolume.value / soundEffectsVolume.maxValue);
-        SoundManager.instance.PlaySound(Sounds.ButtonPress);
+        if (isAwake)
+        {
+            SoundManager.instance.SetVolume(soundEffectsVolume.value / soundEffectsVolume.maxValue);
+            SoundManager.instance.PlaySound(Sounds.ButtonPress);
+        }
     }
 }
